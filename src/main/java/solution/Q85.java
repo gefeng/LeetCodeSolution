146131829d3e3111ff843302061c6ca3ff26eea2
Ge Feng <gefengsteve@gmail.com>
2021-07-04 23:34:01 +0100

    Q85 monotonic stack solution

--- src/main/java/solution/Q85.java
+++ src/main/java/solution/Q85.java
@@ -4,7 +4,9 @@ import annotations.Problem;
 import enums.QDifficulty;
 import enums.QTag;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 @Problem(
         title = "Maximal Rectangle",
@@ -14,6 +16,12 @@ import java.util.Arrays;
 )
 public class Q85 {
     public int maximalRectangle(char[][] matrix) {
+        //return checkAllSubmatrix(matrix);
+        return histogramSolution(matrix);
+    }
+
+    // O(N^3)
+    private int checkAllSubmatrix(char[][] matrix) {
         if(matrix.length == 0) {
             return 0;
         }
@@ -40,5 +48,49 @@ public class Q85 {
         }
 
         return ans;
+    }
+
+    // O(n^2)
+    private int histogramSolution(char[][] matrix) {
+        if(matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int ans = 0;
+
+        int[] h = new int[n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                h[j] = matrix[i][j] == '0' ? 0 : h[j] + 1;
+            }
+
+            ans = Math.max(ans, getMaxArea(h));
+        }
+
+        return ans;
+    }
+
+    /*
+    * find max area of histogram using monotonic stack (increasing stack + -1 sentinel)
+    * */
+    private int getMaxArea(int[] h) {
+        int n = h.length;
+        int max = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+
+        for(int i = 0; i < n; i++) {
+            while(stack.peek() != -1 && h[stack.peek()] >= h[i]) {
+                max = Math.max(max, h[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+
+        while(stack.size() > 1) {
+            max = Math.max(max, h[stack.pop()] * (n - stack.peek() - 1));
+        }
+
+        return max;
     }
 }
