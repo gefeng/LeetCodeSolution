c6064392d306877b4b83d65acc9d8011934fc61e
Ge Feng <gefengsteve@gmail.com>
2021-07-25 00:43:36 +0100

    sweep line

---
+++ src/main/java/solution/Q1109.java
@@ -0,0 +1,41 @@
+package solution;
+
+import annotations.Problem;
+import enums.QDifficulty;
+import enums.QTag;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@Problem(
+        title = "Corporate Flight Bookings",
+        difficulty = QDifficulty.MEDIUM,
+        tag = QTag.ARRAY,
+        url = "https://leetcode.com/problems/corporate-flight-bookings/"
+)
+public class Q1109 {
+    /**
+     * 1. for each triplet [i, j, k]  increase booking[i] by k and decrease booking[j + 1] by -k
+     * 2. sweep line and accumulate total bookings
+     *
+     * Time:  O(N * logN)
+     * Space: O(N)
+     * */
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] res = new int[n];
+
+        for(int[] b : bookings) {
+            res[b[0] - 1] += b[2];
+            if(b[1] < n) {
+                res[b[1]] -= b[2];
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += res[i];
+            res[i] = sum;
+        }
+        return res;
+    }
+}
--- src/main/java/solution/Q253.java
+++ src/main/java/solution/Q253.java
@@ -4,8 +4,7 @@ import annotations.Problem;
 import enums.QDifficulty;
 import enums.QTag;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 @Problem(
         title = "Meeting Rooms II",
@@ -14,10 +13,12 @@ import java.util.PriorityQueue;
         url = "https://leetcode.com/problems/meeting-rooms-ii/"
 )
 public class Q253 {
+    /**
+     * Time:  O(N * logN)
+     * Space: O(N)
+     * */
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> {
-            return a[0] - b[0];
-        });
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
         PriorityQueue<Integer> pQueue = new PriorityQueue<>();
 
@@ -28,5 +29,26 @@ public class Q253 {
         }
 
         return pQueue.size();
+    }
+
+    /**
+     * Time:  O(N * logN)
+     * Space: O(N)
+     * */
+    private int sweepLineSol(int[][] intervals) {
+        int res = 0;
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for(int[] i : intervals) {
+            map.put(i[0], map.getOrDefault(i[0], 0) + 1);
+            map.put(i[1], map.getOrDefault(i[1], 0) - 1);
+        }
+
+        int cnt = 0;
+        for(int v : map.keySet()) {
+            cnt += map.get(v);
+            res = Math.max(res, cnt);
+        }
+        return res;
     }
 }
