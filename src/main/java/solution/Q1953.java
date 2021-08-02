4ccd553dfa15214d20a5086ce3f00c4ef4c4046b
Ge Feng <gefengsteve@gmail.com>
2021-08-02 23:01:55 +0100

    update q1953

--- src/main/java/solution/Q1953.java
+++ src/main/java/solution/Q1953.java
@@ -14,25 +14,22 @@ import java.util.Arrays;
 )
 public class Q1953 {
     /**
-     * 这轮contest败在这题上面，惨痛的教训，greedy算法需要加强
+     * 这轮contest败在这题上面，惨痛的教训，greedy算法需要加强.
+     * EDF (earliest deadline first) problem. The task with max frequency dominate
+     * the result.
      *
      * Time:  O(N * logN)
      * Space: O(logN)
      * */
     public long numberOfWeeks(int[] milestones) {
-        int n = milestones.length;
-
-        if(n == 1) {
-            return 1;
-        }
-
-        Arrays.sort(milestones);
-
         long sum = 0;
-        for(int i = 0; i < n; i++) {
-            sum += milestones[i];
+        int max = 0;
+
+        for(int m : milestones) {
+            max = Math.max(max, m);
+            sum += m;
         }
 
-        return sum - milestones[n - 1] >= milestones[n - 1] ? sum : (sum - milestones[n - 1]) * 2 + 1;
+        return sum - max >= max ? sum : (sum - max) * 2 + 1;
     }
 }
