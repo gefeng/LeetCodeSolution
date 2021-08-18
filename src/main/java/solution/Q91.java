fc2106b8acaaa4d9bac7c81934c282ad2f7198f2
Ge Feng <gefengsteve@gmail.com>
2021-08-18 21:19:40 +0100

    daily challenge

--- src/main/java/solution/Q91.java
+++ src/main/java/solution/Q91.java
@@ -13,59 +13,101 @@ import java.util.HashMap;
         url = "https://leetcode.com/problems/decode-ways/"
 )
 public class Q91 {
+    /**
+     * Time:  O(N)
+     * Space: O(N)
+     * */
     public int numDecodings(String s) {
-        return decodeMemo(s, 0, new HashMap<>());
+        return bottomUpOptimizedSol(s);
     }
 
-    // only need to iterate 1 or 2 steps further
-    // O(n)
-    private int decodeMemo(String s, int start, HashMap<Integer, Integer> memo) {
-        if(start == s.length())
+    private int topDownSol(String s) {
+        return dfs(s, 0, new Integer[s.length()]);
+    }
+
+    private int dfs(String s, int i, Integer[] memo) {
+        int n = s.length();
+
+        if(i == n) {
             return 1;
-
-        if(memo.containsKey(start))
-            return memo.get(start);
-
-        int total = 0;
-        for(int i = start; i < start + 2 && i < s.length(); i++) {
-            if(isValid(s.substring(start, i + 1))) {
-                total += decodeMemo(s, i + 1, memo);
-            }
         }
 
-        memo.put(start, total);
-        return total;
-    }
+        if(memo[i] != null) {
+            return memo[i];
+        }
 
-    private boolean isValid(String s) {
-        if(s.length() > 2)
-            return false;
+        int cnt = 0;
+        int c1 = s.charAt(i) - '0';
 
-        if(s.charAt(0) == '0')
-            return false;
-
-        return Integer.valueOf(s) <= 26;
-    }
-
-    private int dpSolution(String s) {
-        int len = s.length();
-        int[] dp = new int[len];
-
-        if(s.charAt(0) == '0')
+        if(c1 == 0) {
             return 0;
+        } else {
+            cnt += dfs(s, i + 1, memo);
+        }
 
-        dp[0] = 1;
-        for(int i = 1; i < len; i++) {
-            if(s.charAt(i) != '0')
-                dp[i] += dp[i - 1];
-            if(s.charAt(i - 1) != '0' && Integer.valueOf(s.substring(i - 1, i + 1)) <= 26) {
-                if(i == 1)
-                    dp[i]++;
-                else
-                    dp[i] += dp[i - 2];
+        if(i + 1 < n) {
+            int c2 = c1 * 10 + s.charAt(i + 1) - '0';
+            if(c2 > 0 && c2 < 27) {
+                cnt += dfs(s, i + 2, memo);
             }
         }
 
-        return dp[len - 1];
+        return memo[i] = cnt;
+    }
+
+    /**
+     * state:
+     *  dp[i] denotes # ways to decode s[0, i]
+     * transition:
+     *  dp[i] = dp[i - 1] + dp[i - 2]
+     * */
+    private int bottomUpSol(String s) {
+        if(s.charAt(0) == '0') {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for(int i = 1; i < n; i++) {
+            int pre = s.charAt(i - 1) - '0';
+            int cur = s.charAt(i) - '0';
+            if(cur > 0) {
+                dp[i] = dp[i - 1];
+            }
+            if(pre != 0 && pre * 10 + cur < 27) {
+                dp[i] = i == 1 ? dp[i] + 1 : dp[i] + dp[i - 2];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    /**
+     * space optimized, we only need to know dp[i - 1] and dp[i - 2]
+     * */
+    private int bottomUpOptimizedSol(String s) {
+        if(s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        int dpOne = 1;
+        int dpTwo = 0;
+
+        for(int i = 1; i < n; i++) {
+            int pre = s.charAt(i - 1) - '0';
+            int cur = s.charAt(i) - '0';
+            int next = 0;
+            if(cur > 0) {
+                next = dpOne;
+            }
+            if(pre != 0 && pre * 10 + cur < 27) {
+                next = i == 1 ? next + 1 : next + dpTwo;
+            }
+
+            dpTwo = dpOne;
+            dpOne = next;
+        }
+
+        return dpOne;
     }
 }
