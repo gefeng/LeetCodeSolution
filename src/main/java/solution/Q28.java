287a2ce1aea8acb6e1d8f3d336466a61ee9013ec
Ge Feng <gefengsteve@gmail.com>
2021-10-05 10:46:48 +0100

    implement KMP

--- src/main/java/solution/Q28.java
+++ src/main/java/solution/Q28.java
@@ -25,4 +25,58 @@ public class Q28 {
 
         return -1;
     }
+
+    /**
+     * Time:  O(M + N)
+     * Space: O(M + N)
+     * */
+    private int KMP(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(n == 0) {
+            return 0;
+        }
+
+        // preprocess needle
+        // abcxxxaby
+        /*
+         ...abcxxxabc...
+            abcxxxaby
+            000000120
+            i
+             j
+                     0123
+                         4
+        */
+        int[] dp = new int[n];
+
+        for(int i = 0, j = 1; j < n; j++) {
+            if(needle.charAt(i) == needle.charAt(j)) {
+                dp[j] = ++i;
+            } else {
+                if(i > 0) {
+                    i = dp[i - 1];
+                    j--;
+                }
+            }
+        }
+
+        // KMP
+        for(int i = 0, j = 0; i < m; i++) {
+            if(haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+                if(j == n) {
+                    return i - j + 1;
+                }
+            } else {
+                if(j > 0) {
+                    j = dp[j - 1];
+                    i--;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
