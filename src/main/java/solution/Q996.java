66f8b6514f583620f39980ebd1a2f471394d5663
Ge Feng <gefengsteve@gmail.com>
2021-11-21 16:29:44 +0000

    weekly 124

--- src/main/java/solution/Q996.java
+++ src/main/java/solution/Q996.java
@@ -4,6 +4,7 @@ import annotations.Problem;
 import enums.QDifficulty;
 import enums.QTag;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,46 +16,62 @@ import java.util.Set;
 )
 public class Q996 {
     /**
-     * Time:  O(N!)
-     * Space: O(N + #perfect square number)
+     * state:
+     *  dp[i][j] number of valid permutations with used indices representing by mask i and last number nums[j]
+     *
+     * we can remove duplicated permutations by counting the frequency of each elements and divides the answer by it's factorial.
+     *
+     * Time:  O(2 ^ N * N ^ 2)
+     * Space: O(2 ^ N * N)
      * */
-    int ans = 0;
-    Set<Integer> perSqu = new HashSet<>();
     public int numSquarefulPerms(int[] nums) {
-        if(nums.length == 1) {
-            return 0;
-        }
-
-
-        for(long i = 0; i * i <= (long)2e9; i++) {
-            perSqu.add((int)(i * i));
-        }
-
-        dfs(nums, 0, -1, 0);
-
-        return ans;
-    }
-
-    private void dfs(int[] nums, int cur, int pre, int mask) {
-        Set<Integer> seen = new HashSet<>();
+        int ans = 0;
         int n = nums.length;
+        if(n == 1) return 0;
+
+        int[][] dp = new int[1 << n][n];
 
         for(int i = 0; i < n; i++) {
-            if((mask & (1 << i)) == 0 && !seen.contains(nums[i])) {
-                if(pre == -1) {
-                    seen.add(nums[i]);
-                    dfs(nums, cur + 1, nums[i], mask | (1 << i));
-                } else {
-                    if(perSqu.contains(pre + nums[i])) {
-                        seen.add(nums[i]);
-                        if(cur == n - 1) {
-                            ans++;
-                        } else {
-                            dfs(nums, cur + 1, nums[i], mask | (1 << i));
+            dp[1 << i][i] = 1;
+        }
+
+        for(int i = 1; i < (1 << n); i++) {
+            for(int j = 0; j < n; j++) {
+                if((i & (1 << j)) == 0) {
+                    continue;
+                }
+
+                for(int k = 0; k < n; k++) {
+                    if(k != j && (i & (1 << k)) != 0) {
+                        int x = (int)Math.sqrt(nums[k] + nums[j]);
+                        if(x * x == nums[k] + nums[j]) {
+                            dp[i][j] += dp[i ^ (1 << j)][k];
                         }
                     }
                 }
             }
         }
+
+        for(int i = 0; i < n; i++) {
+            ans += dp[(1 << n) - 1][i];
+        }
+
+        Arrays.sort(nums);
+
+        // remove duplicates total_permutations / factorial(repeat_time_of_xi)
+        for(int i = 0; i < n;) {
+            int j = i;
+            while(j < n && nums[j] == nums[i]) j++;
+
+            int fact = 1;
+            for(int k = 1; k <= j - i; k++) {
+                fact *= k;
+            }
+
+            ans /= fact;
+            i = j;
+        }
+
+        return ans;
     }
 }
