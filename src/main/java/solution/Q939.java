39ddddbcc5133aa0a482ca900e18b9c6d54e7e4e
Ge Feng <gefengsteve@gmail.com>
2021-12-02 12:36:09 +0000

    update q939

--- src/main/java/solution/Q939.java
+++ src/main/java/solution/Q939.java
@@ -17,6 +17,12 @@ import java.util.Set;
 )
 public class Q939 {
     /**
+     * Instead of using Map<Integer, Set<Integer>> to build lookup table,
+     * we can just use a Set<Long> where for each 64bits, left most 32bits
+     * is for x and right most 32bits for y.
+     *
+     * This is a slightly improvement on run time since map is more expensive.
+     *
      * Time:  O(N ^ 2)
      * Space: O(N)
      * */
@@ -24,17 +30,17 @@ public class Q939 {
         int ans = Integer.MAX_VALUE;
         int n = points.length;
 
-        Map<Integer, Set<Integer>> map = new HashMap<>();
+        Set<Long> set = new HashSet<>();
 
         for(int[] p : points) {
-            map.computeIfAbsent(p[0], k -> new HashSet<>()).add(p[1]);
+            set.add((long)p[0] << 32 | p[1]);
         }
 
         for(int[] p1 : points) {
             for(int[] p2 : points) {
                 // fix top right and bottom left
                 if(p1[0] != p2[0] && p1[1] != p2[1]) {
-                    if(map.get(p1[0]).contains(p2[1]) && map.get(p2[0]).contains(p1[1])) {
+                    if(set.contains((long)p1[0] << 32 | p2[1]) && set.contains((long)p2[0] << 32 | p1[1])) {
                         ans = Math.min(ans, Math.abs(p1[0] - p2[0]) * Math.abs(p1[1] - p2[1]));
                     }
                 }
