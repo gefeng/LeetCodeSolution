32f8bfa22091817575d629b0f41d2df6c15c8e01
Ge Feng <gefengsteve@gmail.com>
2022-01-11 05:02:45 +0000

    update q719

--- src/main/java/solution/Q719.java
+++ src/main/java/solution/Q719.java
@@ -14,61 +14,32 @@ import java.util.Arrays;
 )
 public class Q719 {
     /**
-     * Time:  O(N * logN * log(max - min))
+     * Time:  O(N * log(max - min))
      * Space: O(logN)
      * */
+    private int distanceLessThan(int[] nums, int dist) {
+        int left = 0;
+        int count = 0;
+        for(int right = 0; right < nums.length; right++) {
+            while(nums[right] - nums[left] > dist)
+                left++;
+            count += (right - left);
+        }
+        return count;
+    }
     public int smallestDistancePair(int[] nums, int k) {
-        int n = nums.length;
-
         Arrays.sort(nums);
-
-        int lo = 0;
-        int hi = nums[n - 1] - nums[0];
-        int ans = 0;
-        while(lo <= hi) {
-            int mid = lo + hi >> 1;
-
-            int cnt = lessEqual(nums, mid);
-
-            if(cnt < k) {
-                lo = mid + 1;
-            } else if(cnt >= k) {
-                ans = mid;
-                hi = mid - 1;
-            }
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+        int mid = 0;
+        while(left < right) {
+            mid = left + (right - left) / 2;
+            int count = distanceLessThan(nums, mid);
+            if(count < k)
+                left = mid + 1;
+            else
+                right = mid;
         }
-
-        return ans;
-    }
-
-    private int lessEqual(int[] nums, int t) {
-        int n = nums.length;
-        int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            int idx = lowerBound(nums, i, nums[i] + t);
-            if(idx != -1)
-                cnt += idx - i;
-        }
-
-        return cnt;
-    }
-
-    private int lowerBound(int[] nums, int st, int t) {
-        int idx = -1;
-        int lo = st;
-        int hi = nums.length - 1;
-
-        while(lo <= hi) {
-            int mid = lo + hi >> 1;
-
-            if(nums[mid] <= t) {
-                idx = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-
-        return idx;
+        return left;
     }
 }
