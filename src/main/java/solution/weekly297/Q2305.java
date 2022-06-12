c9a29b835084ddfc3238ff1d99f6a4f916760664
Ge Feng <gefengsteve@gmail.com>
2022-06-12 09:30:27 +0100

    q2305 improve time complexing by precalculating sum for each mask

--- src/main/java/solution/weekly297/Q2305.java
+++ src/main/java/solution/weekly297/Q2305.java
@@ -12,57 +12,33 @@ import enums.QTag;
 )
 public class Q2305 {
     /**
-     * Time:  O(2 ^ N * 2 ^ N * K * N)
+     * Time:  O(2 ^ N * 2 ^ N * K)
      * Space: O(2 ^ N * K)
      * */
     public int distributeCookies(int[] cookies, int k) {
         int n = cookies.length;
         int[][] dp = new int[k + 1][1 << n];  // minimum unfairness of first i children
+        int[] sum = new int[1 << n];
 
-        for(int i = 0; i < (1 << n); i++) {
-            int sum = 0;
+        for(int i = 1; i < (1 << n); i++) {
             for(int j = 0; j < n; j++) {
                 if((i & (1 << j)) != 0) {
-                    sum += cookies[j];
+                    sum[i] += cookies[j];
                 }
             }
-            dp[1][i] = sum;
+            dp[1][i] = sum[i];
         }
 
         for(int i = 2; i <= k; i++) {
             for(int j = 0; j < (1 << n); j++) {
                 int min = Integer.MAX_VALUE;
-                for(int m = 0; m < (1 << n); m++) {
-                    int sum = getSum(cookies, j, m);
-                    if(sum != -1) {
-                        min = Math.min(min, Math.max(dp[i - 1][m], sum));
-                    }
+                for(int m = j; m > 0; m = (m - 1) & j) {
+                    min = Math.min(min, Math.max(dp[i - 1][m], sum[j ^ m]));
                 }
-
                 dp[i][j] = min;
             }
         }
 
         return dp[k][(1 << n) - 1];
-    }
-
-    private int getSum(int[] A, int cur, int pre) {
-        int n = A.length;
-        int sum = 0;
-
-        for(int i = 0; i < n; i++) {
-            int b1 = cur & (1 << i);
-            int b2 = pre & (1 << i);
-
-            if(b2 != 0 && b1 == 0) {
-                return -1;
-            }
-
-            if(b2 == 0 && b1 != 0) {
-                sum += A[i];
-            }
-        }
-
-        return sum;
     }
 }
